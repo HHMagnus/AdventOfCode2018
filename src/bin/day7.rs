64a5d03ec6ac5821e60634a0c550de03a6878351
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-06-06 08:00:27 +0200

    day 7 part 2

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -17,6 +17,8 @@ fn main() {
 		depent.entry(dep).or_insert(Vec::new());
 	}
 
+	let part2depents = depent.clone();
+
 	let mut part1 = String::new();
 	
 	while depent.len() != 0 {
@@ -36,4 +38,55 @@ fn main() {
 	}
 
 	println!("Day 7 part 1: {}", part1);
+
+	let mut depent = part2depents;
+
+	let mut workers = vec![(0, '-'); 5];
+
+	let mut part2 = 0;
+
+	while depent.len() != 0 {
+		for i in 0..workers.len() {
+			workers[i].0 -= 1;
+			if workers[i].0 == 0 {
+				depent.remove(&workers[i].1);
+
+				let keys = depent.keys().map(|x| *x).clone().collect::<Vec<_>>();
+				for d in keys {
+					let new = depent.get(&d).unwrap().iter().filter(|&&x| x != workers[i].1).map(|&x| x).collect::<Vec<_>>();
+					*depent.get_mut(&d).unwrap() = new;
+				}
+			}
+		}
+
+		part2 += 1;
+		
+		if workers.iter().filter(|&&x| x.0 <= 0).count() == 0 {
+			continue;
+		}
+
+		while workers.iter().filter(|&&x| x.0 <= 0).count() > 0 {
+			let mut dep0s = depent
+				.iter()
+				.filter(|(_, v)| v.len() == 0)
+				.filter(|(c, _)| !workers.iter().any(|(_, c0)| c == &c0))
+				.map(|(&k, _)| k)
+				.collect::<Vec<_>>();
+			dep0s.sort();
+			if dep0s.is_empty() {
+				break;
+			}
+
+			let curr = dep0s[0];
+
+			let idx = workers.iter().enumerate().find(|(_, &x)| x.0 <= 0).unwrap().0;
+			workers[idx] = (time(curr), curr);
+		}
+	}
+	
+	println!("Day 7 part 1: {}", part2-1);
+}
+
+fn time(c: char) -> i32 {
+	60 + c as i32 - 'A' as i32 + 1
 }
