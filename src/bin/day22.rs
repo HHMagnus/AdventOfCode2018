9971c65b4fb0c88c64b0360a95469537279578d6
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-06-15 08:45:21 +0200

    day 22 invalid changes

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -1,10 +1,64 @@
-use std::fs;
+use std::{collections::{BinaryHeap, HashMap}, fs};
 use cached::proc_macro::cached;
 
+#[derive(Debug, Clone, Copy)]
 enum RegionType {
 	Rocky,
 	Wet,
 	Narrow,
+}
+
+#[derive(PartialEq, Eq, Clone, Copy, PartialOrd, Hash, Debug)]
+enum Gear {
+	Torch,
+	ClimbingGear,
+	Neither,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+struct State {
+	cost: u128,
+	pos: (u128, u128),
+	equipment: Gear,
+}
+
+impl Ord for State {
+	fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+		other.cost.cmp(&self.cost)
+			.then_with(|| self.pos.cmp(&other.pos))
+	}
+}
+
+impl PartialOrd for State {
+	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+		match other.cost.partial_cmp(&self.cost) {
+			Some(core::cmp::Ordering::Equal) => {}
+			ord => return ord,
+		}
+		match other.pos.partial_cmp(&self.pos) {
+			Some(core::cmp::Ordering::Equal) => {}
+			ord => return ord,
+		}
+		other.equipment.partial_cmp(&self.equipment)
+	}
+}
+
+impl State {
+	fn move_to(&self, pos:(u128, u128), dest: RegionType) -> Vec<State> {
+		match dest {
+			RegionType::Rocky => vec![self.moved(pos, Gear::Torch), self.moved(pos, Gear::ClimbingGear)],
+			RegionType::Wet => vec![self.moved(pos, Gear::ClimbingGear), self.moved(pos, Gear::Neither)],
+			RegionType::Narrow => vec![self.moved(pos, Gear::Torch), self.moved(pos, Gear::Neither)],
+		}
+	}
+	
+	fn moved(&self, pos:(u128, u128), equipment: Gear) -> State {
+		State {
+			cost: self.cost + if self.equipment == equipment { 1 } else { 8 },
+			pos,
+			equipment
+		}
+	}
 }
 
 fn main() {
@@ -18,6 +72,8 @@ fn main() {
 
 	let mut part1 = 0;
 
+	let mut map = HashMap::new();
+
 	for y in 0..=target.1 {
 		for x in 0..=target.0 {
 			let erosion_level = geologic_index((x, y), depth, target);
@@ -29,24 +85,85 @@ fn main() {
 				_ => unreachable!("My math is off"),
 			};
 
-			let risk = match typ {
-				RegionType::Rocky => 0,
-				RegionType::Wet => 1,
-				RegionType::Narrow => 2,
-			};
+			map.insert((x, y), typ);
 			
-			part1 += risk;
+			if y <= target.1 && x <= target.0 {
+				let risk = match typ {
+					RegionType::Rocky => 0,
+					RegionType::Wet => 1,
+					RegionType::Narrow => 2,
+				};
 
-			match typ {
-				RegionType::Narrow => print!("|"),
-				RegionType::Rocky => print!("."),
-				RegionType::Wet => print!("="),
+				part1 += risk;
 			}
 		}
-		println!("");
 	}
 
 	println!("Day 22 part 1: {}", part1);
+
+	let part2 = part2(&map, target);
+
+	println!("Day 22 part 2: {}", part2);
+}
+
+fn part2(map: &HashMap<(u128, u128), RegionType>, target: (u128, u128)) -> u128 {
+
+	let mut visited = HashMap::new();
+	let mut states = BinaryHeap::new();
+
+	let start = State {
+		cost: 0,
+		pos: (0, 0),
+		equipment: Gear::Torch
+	};
+
+	states.push(start);
+
+	while let Some(next) = states.pop() {
+		let vkey = (next.pos, next.equipment);
+		if visited.contains_key(&vkey) && visited[&vkey] <= next.cost {
+			continue;
+		}
+		visited.insert(vkey, next.cost);
+
+		if next.pos == target && next.equipment == Gear::Torch {
+			return next.cost;
+		}
+
+		let neighbours = neighbours(next.pos, map);
+
+		for neigh in neighbours {
+			for x in next.move_to(neigh, map[&neigh]) {
+				states.push(x);
+			}
+		}
+	}
+
+	unreachable!("No path found");
+}
+
+fn neighbours(pos: (u128, u128), map: &HashMap<(u128, u128), RegionType>) -> Vec<(u128, u128)> {
+	let mut vec = Vec::new();
+
+	if pos.0 > 0 {
+		vec.push((pos.0 - 1, pos.1));
+	}
+
+	if pos.1 > 0 {
+		vec.push((pos.0, pos.1 - 1));
+	}
+
+	let xplus = (pos.0 + 1, pos.1);
+	if map.contains_key(&xplus) {
+		vec.push(xplus);
+	}
+
+	let yplus = (pos.0, pos.1 + 1);
+	if map.contains_key(&yplus) {
+		vec.push(yplus);
+	} 
+
+	vec
 }
 
 #[cached]
